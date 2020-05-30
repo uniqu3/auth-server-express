307fd2d06185f643b0b19ee68029d079382c097b
Goran Ilic (uniqu3) <uniqu3e@gmail.com>
2020-05-30 15:59:40 +0200

    dummy readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+#### Simple auth server with mongodb, express and jwt
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const passportServices = require('./services/passport');
+const passportService = require('./services/passport');
 const Authentication = require('./controllers/authentication');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
